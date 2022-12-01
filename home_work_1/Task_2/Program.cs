adfddb3c3c113e6d71e472a3b6b064ce9985d549
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-01 14:49:51 +0300

    Выполнил задачу 2

--- home_work_1/Task_2/Program.cs
+++ home_work_1/Task_2/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+Console.WriteLine($"max {n}, min {m}");
+if (n < m)
+Console.WriteLine($"max {m}, min {n}");
+if (n == m)
+Console.WriteLine($"Ошибка");
