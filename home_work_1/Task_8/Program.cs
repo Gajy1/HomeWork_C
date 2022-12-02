08177f4d7d6823f440f92d25409c3fde465de46e
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-02 14:42:37 +0300

    Выполнил задачу 8

--- home_work_1/Task_8/Program.cs
+++ home_work_1/Task_8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+int i = 0;
+while(i <= n)
+{
+  if(m % 2 == 0)
+  Console.Write($" {m}");
+  i = i + 1; 
+  m = m + 1; 
+
+}
