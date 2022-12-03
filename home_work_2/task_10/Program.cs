7d82ceea23c00e2c172d0e5b4847e2a53e3c8b08
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-03 09:33:45 +0300

    Выполнил задачу 10

--- home_work_2/task_10/Program.cs
+++ home_work_2/task_10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите трехзначное число");
+string? s = Console.ReadLine();
+int m = s.Length;
+if(m != 3)
+Console.WriteLine("Вы ввели не трехзначное чилло");
+else Console.WriteLine(Convert.ToInt32(s[1].ToString()));
+
