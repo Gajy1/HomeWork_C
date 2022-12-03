98fc76526d2ea8e14634bdb3ff49caf1d5d4360c
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-03 11:11:52 +0300

    Выполнил задачу 15

--- home_work_2/task_15/Program.cs
+++ home_work_2/task_15/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите цыфру от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n == 6 || n == 7) Console.WriteLine("yes");
+if(n == 1 || n == 2 || n == 3 || n == 4 || n == 5) Console.WriteLine("No");
+if(n > 7 || n < 1) Console.WriteLine("Введите цыфру от 1 до 7");
