3f41e9e7e9095125a0550a02e90628e5f68b2a5e
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-05 13:23:54 +0300

    Исправил задачу 13

--- home_work_2/task_13/Program.cs
+++ home_work_2/task_13/Program.cs
@@ -1,13 +1,20 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите чило");
 int n = Convert.ToInt32(Console.ReadLine());
-int x = (n / 10) % 10;
-int y = (n / 100) % 10;
-int z = n % 10;
-int r = (n / 1000) % 10;
+int i = 10;
+int x = n % 10;
 if(n < 100) Console.WriteLine("Третьего числа нет");
-if(n > 99 && n < 1000) Console.WriteLine(z);
-if(n > 999 && n < 10000) Console.WriteLine(x);
-if(n > 9999 && n < 100000) Console.WriteLine(y);
-if(n > 99999 && n < 1000000) Console.WriteLine(r);
-if(n > 999999) Console.WriteLine("Не надо вводить такие огромные числа");
+if(n >= 100 && n < 1000) Console.WriteLine(x);
+else
+while(n >= 1000){
+   n = n / i;
+   if(n < 1000)Console.WriteLine(n % 10);
+}
+
+
+
+
+
+
+
+
