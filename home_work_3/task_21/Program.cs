39e8b97e3fcd4d0e2b2ffca2b15caf4f441027d8
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-08 22:27:11 +0300

    Выполнил задачу 21

--- home_work_3/task_21/Program.cs
+++ home_work_3/task_21/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Ведите координаты первой точки(x,y,z):");
+double x = Convert.ToDouble(Console.ReadLine());
+double y = Convert.ToDouble(Console.ReadLine());
+double z = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ведите координаты второй точки(x,y,z):");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+
+double l = ((x1 - x)*(x1 - x) + (y1 - y)*(y1 - y) + (z1 - z)*(z1 - z));
+
+double result = (Math.Sqrt(l));
+Console.WriteLine("");
+Console.WriteLine(result);
+
