dfca4699e863e745d358d7df13a568ecc2608fdf
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-08 20:29:39 +0300

    9

--- home_work_3/task_19/Program.cs
+++ home_work_3/task_19/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+string s = Console.ReadLine();
+int a = s.Length;
+if(a != 5)
+Console.WriteLine("Вы ввели не пятизначное число");
+if(s[0] == s[4] && s[1] == s[3])
+Console.WriteLine("Yes");
+if(a == 5 && s[0] != s[4] && s[1] != s[3])
+Console.WriteLine("No");
+
