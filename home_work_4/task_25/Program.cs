b354d60368b9fff9031f33f04c308b93b92f1143
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-12 10:09:55 +0300

    Completed task_25

--- home_work_4/task_25/Program.cs
+++ home_work_4/task_25/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите первое число: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double B = Convert.ToDouble(Console.ReadLine());
+double i = 2;
+double C = Math.Abs(B);
+double result = A;
+for( i = 2; i < C + 1; i++){
+result = result * A; 
+}
+Console.WriteLine(result);
