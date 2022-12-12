256d82eff2037a710db1d77e20b9747c83217f9e
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-12 11:20:21 +0300

    Completed task_27

--- home_work_4/task_27/Program.cs
+++ home_work_4/task_27/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Ввудите число: ");
+string n = Console.ReadLine();
+int s = n.Length; 
+int i = 1;
+int x = Convert.ToInt32(n[0].ToString());
+for(i = 1; i < s; i++)
+{
+    x = x + Convert.ToInt32(n[i].ToString());
+}
+Console.WriteLine(x);
