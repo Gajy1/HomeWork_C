61eb112e1a73c44d65da3ec628715c7b538b6f88
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-12 14:28:14 +0300

    Completed task_29

--- home_work_4/task_29/Program.cs
+++ home_work_4/task_29/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Сколько элементов будет в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] collection)
+{
+    int l = collection.Length;
+    int i = 0;
+
+    while (i < l)
+    {
+       collection[i] = Convert.ToInt32(Console.ReadLine());
+       i++;
+    }
+}
+
+
+
+int [] array = new int[n];
+Console.WriteLine();
+
+Console.WriteLine("Введите элементы массива:");
+
+FillArray(array);
+Console.Write($"[{string.Join(", ", array)}]");
