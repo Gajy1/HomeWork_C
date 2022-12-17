ecc6a372b925943df675bb9289e5649a5108ea55
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-17 11:36:16 +0300

    completed task_34

---
+++ home_work_5/task_34/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+void FillArray(int[] collection)
+{
+    int l = collection.Length;
+    int i = 0;
+
+    while (i < l)
+    {
+       collection[i] = new Random().Next(100, 1000);
+       i++;
+    }
+}
+
+void Fr(int[] array)
+{
+    int summ = 0;
+  for(int i = 0; i < array.Length; i++)
+  {
+    if(array[i] % 2 == 0)
+    summ++;
+  }
+  Console.WriteLine(summ);
+}
+
+int [] array = new int[12];
+FillArray(array);
+Console.Write($"[{string.Join(", ", array)}]");
+Console.WriteLine();
+Fr(array);
