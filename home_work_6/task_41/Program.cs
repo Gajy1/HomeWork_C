1384ae7c5ed2135232fe07be699f78293bfb3ee6
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-19 16:27:40 +0300

    Completed task_41

---
+++ home_work_6/task_41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите числа");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int summ = 0;
+foreach(int element in array)
+if(element > 0)
+{
+ summ++;
+ 
+}
+
+Console.WriteLine(summ);
