1278bee2c086e623fa4138e71c21c6c034aab6be
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2022-12-21 14:30:02 +0300

    Completed task_52

---
+++ home_work_7/task_52/Program.cs
@@ -0,0 +1,44 @@
+﻿
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void InputSumm(int[,] matrix)
+{
+    double n = 0;
+    int x = 1; 
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+           x = x + i; 
+           n = n + matrix[i, j];
+           i++;
+           n = n + matrix[i, j];
+            
+        }
+        Console.Write($"{n/4}; ");
+        n = 0;
+    }
+        Console.WriteLine();
+    
+}
+
+
+
+Console.Clear();
+
+int[] size = {4,4};
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine();
+InputSumm(matrix);
