aa2c2375fb7c7a5557b836d3c777b5e55e583e4c
Gajy <Gaji1@yandex.rugit config user.emailgit config --global user.email Gaji1@yandex.ru>
2023-01-10 10:47:13 +0300

    Completed task_68

---
+++ home_work_9/task_68/Program.cs
@@ -0,0 +1,18 @@
+﻿ int Acc(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Acc(n - 1, 1);
+    else
+      return Acc(n - 1, Acc(n, m - 1));
+}
+
+
+Console.Clear();
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Acc(n, m));
